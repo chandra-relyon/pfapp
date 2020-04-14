671966abfad50da40eec0e496272af3bb8e8047a
chandra-relyon <chandra.v@relyonsoft.com>
2020-04-14 17:26:14 +0530

    added edp_pf action

--- app/controllers/pfs_controller.rb
+++ app/controllers/pfs_controller.rb
@@ -16,6 +16,19 @@ end
     def show
         @pf=Pf.find(params[:id])
     end
+    def edit
+        @pf=Pf.find(params[:id])
+       end
+    def update
+        @pf=Pf.find(params[:id])
+        if @pf.update(pf_params) 
+            flash[:notice]="pf is sucessfully completed"
+        redirect_to pf_path(@pf)
+        else 
+            render 'edit'
+        end
+    end
+
  private
   def pf_params
     params.require(:pf).permit(:name, :pfamount)
